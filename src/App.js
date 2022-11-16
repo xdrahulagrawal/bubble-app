1649a2ff191e1fd93fa63547550c9b272db5fe62
Rahul Agrawal <xdrahulagrawal@gmail.com>
2022-11-16 22:03:20 +0530

    bubble created user click anywhere in screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import './assests/styles/App.css'
 
 function App() {
+  const [points, setPoints] = useState([])
+  const _handleClick = (e) => {
+    const { clientX, clientY } = e
+    setPoints([...points, { x: clientX, y: clientY }])
+  }
+
   return (
-    <div className="App">
-    
+    <div onClick={_handleClick} className='main-container'>
+      {points.map((point, index) => (
+        <div key={index} className='points' style={{ left: point.x + "px", top: point.y + "px" }}></div>
+      ))}
     </div>
   );
 }
