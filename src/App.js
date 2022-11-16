5aadc5297381fc74c171594c29eea5849497d91f
Rahul Agrawal <xdrahulagrawal@gmail.com>
2022-11-16 22:23:38 +0530

    redo btn functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assests/styles/App.css'
 
 function App() {
   const [points, setPoints] = useState([])
+  const [popped, setPopped] = useState([])
   const _handleClick = (e) => {
     const { clientX, clientY } = e
     setPoints([...points, { x: clientX, y: clientY }])
@@ -10,13 +11,25 @@ function App() {
 
   const _handleUndoBtn = () => {
     const newPoints = [...points]
-    newPoints.pop();
+    const poopedPoint = newPoints.pop();
+    if (!poopedPoint) return
     setPoints(newPoints)
+    setPopped([...popped, poopedPoint])
+  }
+
+  const _handleRedoBtn = () => {
+    console.log('asdf');
+    const newPopped = [...popped]
+    const poppedPoint = newPopped.pop();
+    if (!poppedPoint) return
+    setPoints([...points, poppedPoint])
+    setPopped(newPopped)
   }
 
   return (
     <>
       <button onClick={_handleUndoBtn} className='primary'>Undo</button>
+      <button onClick={_handleRedoBtn} className={popped.length === 0 ? 'disable' : 'primary'} disabled={popped.length === 0 ? true : false}>Redo</button>
       <div onClick={_handleClick} className='main-container'>
         {points.map((point, index) => (
           <div key={index} className='points' style={{ left: point.x + "px", top: point.y + "px" }}></div>
